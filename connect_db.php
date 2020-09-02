c7cc47b5bcbaa7c16371274f015501b4447de308
Rabbee <abdulgonirabbee@gmail.com>
2020-09-02 19:58:23 +0600

    update

--- connect_db.php
+++ connect_db.php
@@ -1,10 +1,10 @@
 <?php
 function database_connection($setup=false){
-	$host='ec2-3-81-240-17.compute-1.amazonaws.com';
-	$user='anodynuytkbili';
-	$password='27b822e5f66dc85cb5af0334f92386433f7e005b091dcd4e160255ca8365aae6';
-	$database='de8etmmpigihbv';
-	$port='5432';
+	$host='ec2-54-144-177-189.compute-1.amazonaws.com';
+	$user='dcodtoohxorubg';
+	$password='87fc80a49bb0df19bf4f2d425d8573d8f2b2171a33f41127af85b23bb4d13a54';
+	$database='d5e210eje2u12n';
+	$port = '5432';
 	
 	if($setup == true){
 		$connection = new mysqli($host,$user,$password,$port);
@@ -15,10 +15,7 @@ function database_connection($setup=false){
 	if($connection->connect_error){
 		die("Connection Failed!!".$connection->connect_error);
 	}
-	if($connection){
-        die("successfull");
-    }
-//	return $connection;
+	return $connection;
 }
 
 
