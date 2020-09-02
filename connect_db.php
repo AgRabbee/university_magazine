d8de33837375a0f459b4b9a011e0b68fd5a69653
Rabbee <abdulgonirabbee@gmail.com>
2020-09-02 19:32:47 +0600

    connect_db updated

--- connect_db.php
+++ connect_db.php
@@ -14,7 +14,8 @@ function database_connection($setup=false){
 
 	if($connection->connect_error){
 		die("Connection Failed!!".$connection->connect_error);
-	}else{
+	}
+	if($connection){
 	    echo "successfull";
     }
 //	return $connection;
