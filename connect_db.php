df8560b26cd6210173a82f5d4c0bac56d41aa575
Rabbee <abdulgonirabbee@gmail.com>
2020-09-02 17:13:27 +0600

    heroku database credentials updated

--- connect_db.php
+++ connect_db.php
@@ -1,9 +1,9 @@
 <?php
 function database_connection($setup=false){
-	$host='localhost';
-	$user='root';
-	$password='';
-	$database='ewsd-group-03';
+	$host='ec2-3-81-240-17.compute-1.amazonaws.com';
+	$user='anodynuytkbili';
+	$password='27b822e5f66dc85cb5af0334f92386433f7e005b091dcd4e160255ca8365aae6';
+	$database='de8etmmpigihbv';
 	
 	if($setup == true){
 		$connection = new mysqli($host,$user,$password);
